9c60de847fd67cb0d81d620450d6a99821af3497
Eugen Wintersberger <eugen.wintersberger@desy.de>
2017-06-13 12:00:13 +0200

    Fixed path for input files

--- WindowsUtils.cmake
+++ WindowsUtils.cmake
@@ -3,6 +3,6 @@
 # configuration for which the configuration was set up is passed.
 #
 if(CMAKE_HOST_SYSTEM_NAME MATCHES Windows)
-    configure_file(make.bat.in ${PROJECT_BINARY_DIR}/make.bat @ONLY)
-    configure_file(test.bat.in ${PROJECT_BINARY_DIR}/test.bat @ONLY)
+    configure_file(${CMAKE_CURRENT_LIST_DIR}/make.bat.in ${PROJECT_BINARY_DIR}/make.bat @ONLY)
+    configure_file(${CMAKE_CURRENT_LIST_DIR}/test.bat.in ${PROJECT_BINARY_DIR}/test.bat @ONLY)
 endif()
